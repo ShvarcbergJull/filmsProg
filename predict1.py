59e15529f8388c9a0c19c6c513fa9594c281d045
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2019-06-06 15:51:19 +0800

    Update predict1.py

--- predict1.py
+++ predict1.py
@@ -22,18 +22,3 @@ s=pickle.dumps(clf1)
 ff=open("save_rat.dat","wb")
 ff.write(s)
 ff.close()
-
-'''
-for line in lines:
-    cols=line.split()
-    if (cols):
-        EX = [cols[0], cols[1], cols[2], cols[3], cols[4]]
-        r1 = clf1.predict([EX])
-        z = float(r1[0])
-        if abs(z - float(cols[5]))<=1:
-            j += 1
-        print(str(cols[0])+' '+str(cols[1])+' '+str(cols[2])+' '+str(cols[3])+' '+str(cols[4])+' '+str(cols[5])+' '+str(z))
-        i += 1
-       
-print(str(j/i))
-'''
