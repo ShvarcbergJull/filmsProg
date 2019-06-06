dd81ce1199b81382716c4a6938a87b71d4e11099
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2019-06-06 15:44:08 +0800

    Update index.py

--- index.py
+++ index.py
@@ -13,6 +13,7 @@ dic = {}
 
 out = ""
 
+#для обработки файла с прибылью
 for line in lines:
     cols=line.split(",")
     genr = cols[4].split("|")
@@ -28,6 +29,24 @@ for line in lines:
         #print(str(dic.setdefault(cols[1], i)) + ' ' + str(dic.setdefault(cols[2], i)) + ' ' + str(dic.setdefault(cols[3], i)) + ' ' + str(dic.setdefault(cols[4], i)) + ' ' + str(dic.setdefault(cols[5], i)) + ' ' + str(cols[6]) + ' ' + str(cols[7]))
         print(out)
         out = ""
+
+#для обработки файла с ретингом
+        '''
+for line in lines:
+    cols=line.split(",")
+    genr = cols[5].split("|")
+    if (cols):
+        for n in range(1,5,1):
+            out += (str(dic.setdefault(cols[n], i)) + ' ')
+            if dic[cols[n]] == i:
+                i += 1
+        out += (str(dic.setdefault(genr[0], i)) + ' ')
+        if dic[genr[0]] == i:
+            i += 1
+        out += (str(cols[6]))
+        print(out)
+        out = ""
+        '''
         
 file = open("dictionary.dat", "w")
 
